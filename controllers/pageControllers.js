af5e502428d4353cb06187747c1acc7a4268576f
FerdHDev <ferdinand1475joel@gmail.com>
2025-07-25 12:09:23 +0100

    feat: client location successfull, login email next

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,12 +1,26 @@
-import uaParser from 'ua-parser-js';
 import asyncHandler from "express-async-handler";
+import { IPinfoWrapper } from "node-ipinfo";
+import * as UAParser from "ua-parser-js";
 
-const indexPage = asyncHandler((req, res) => {
+const indexPage = asyncHandler(async (req, res) => {
+    /* if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(
+             (position) => {
+                 console.log(position.coords.latitude, position.coords.longitude);
+             }
+         );
+     } else {
+         console.log("This browser is not compatible with navigator.geolocation")
+     } */
 
-    const userAgent = req.headers['user-agent'];
-    const parser = new uaParser(userAgent);
-    console.log(parser)
-    res.status(200).send("Welcome to my home page");
+    const parser = new UAParser.UAParser(req.headers["user-agent"]);
+    const device = parser.getDevice();
+    const deviceType = device.vendor
+    console.log(deviceType)
+    const ipInfoWrapper = new IPinfoWrapper("dd68644026218a");
+    const ipInfo = await ipInfoWrapper.lookupIp("197.210.84.32")
+    console.log(ipInfo);
+    res.send("Good")
 })
 
 export { indexPage } 
